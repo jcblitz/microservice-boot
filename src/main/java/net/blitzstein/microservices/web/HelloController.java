a49e83633ef3eba6d3455fe0cfad56d77b1e2706
Jared Blitzstein <mailing-list@blitzstein.net>
2017-01-31 14:15:21 -0500

    updated hello message after adding slack notifications

--- src/main/java/net/blitzstein/microservices/web/HelloController.java
+++ src/main/java/net/blitzstein/microservices/web/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
     @RequestMapping(value = "/", produces = MimeTypeUtils.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public Greeting hello() {
-        return new Greeting("Hello from heroku!!!");
+        return new Greeting("Hello from CI world!");
     }
 
 }
