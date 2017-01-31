757a69735800eb7f1510164c4b79fc033bd92cd8
Jared Blitzstein <mailing-list@blitzstein.net>
2017-01-31 11:21:34 -0500

    removed unused code

--- src/main/java/net/blitzstein/microservices/LearningApplication.java
+++ src/main/java/net/blitzstein/microservices/LearningApplication.java
@@ -1,16 +1,11 @@
 package net.blitzstein.microservices;
 
-import net.blitzstein.microservices.web.ResidualValueController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -34,17 +29,5 @@ public class LearningApplication {
 				registry.addMapping("/**/*");
 			}
 		};
-	}
-
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Spring REST Sample with Swagger")
-				.description("Spring REST Sample with Swagger")
-				.termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-				.license("Apache License Version 2.0")
-				.licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
-				.version("2.0")
-				.build();
 	}
 }
